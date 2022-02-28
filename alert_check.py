15c6a674e79e76cb1ac2e08423e6565e396f2398
MindlessTux <mindlesstux@users.noreply.github.com>
2022-02-28 01:09:03 -0500

    UNTESTED: Added regex filtering

--- alert_check.py
+++ alert_check.py
@@ -8,7 +8,7 @@ aircraft_json_url = "https://mindlesstux.com/skyaware/data/aircraft.json"
 # Dictionary of points to alrt around
 # "pointname": [lat, long, altitude low, altitude high, radius/miles, point text friendly name, regex flight]
 alert_cords = {
-    "KRDU": [35.879204, -78.787162, 0, 1000000, 5, "RDU Airport", ()],
+    "KRDU": [35.879204, -78.787162, 0, 1000000, 5, "RDU Airport", ['','']],
     "Point_2": [0, 0, 0, 1000000, 30, "Far Point", ()],
 }
 
@@ -40,6 +40,7 @@ debug_more = False
 import apprise
 from geopy import distance
 import json
+import re
 from string import Template
 import time
 from urllib.request import urlopen
@@ -83,7 +84,7 @@ if debug_more:
 for name, point in alert_cords.items():
     if debug_logs:
         print(name)
-        print("[lat, long, alt_low, alt_high, radius (km), friendly name")
+        print("[lat, long, alt_low, alt_high, radius (km), friendly name, [regex, search, strings]")
         print(point)
     # Data for each point alerting around
     coords_center = (point[0], point[1])
@@ -91,10 +92,15 @@ for name, point in alert_cords.items():
     altitude_limit_high = point[3]
     radius_limit = point[4]
     friendly_name = point[5]
+    regex_strings = point[6]
 
     # Loop through all aircraft
     tmp={}
     for aircraft in aircraft_data['aircraft']:
+        # Add a blank flight info if it does not exist
+        if "flight" not in aircraft.keys():
+            aircraft['flight'] = "        "
+
         # Do we have lat/long for the aircraft? If not assume its out of the area we care about
         if "lat" in aircraft.keys() and "lon" in aircraft.keys():
             coords_target = (aircraft['lat'], aircraft['lon'])
@@ -108,12 +114,19 @@ for name, point in alert_cords.items():
         else:
             result_altitude = [True, 0]
 
-        # Is the plane in the point radius for alerting?
-        if result_radius[0] == True and result_altitude[0] == True:
-            # Add a blank flight info if it does not exist
-            if "flight" not in aircraft.keys():
-                aircraft['flight'] = "        "
+        # Check the regex strings, if blank, mark true
+        if regex_strings == []:
+            result_altitude = [True, 0]
+        else:
+            # Default false if we have a string
+            result_regex = [False,0]
+            # Loop through all strings and try to match, if so set True
+            for resrchstr in regex_strings:
+                if re.match(resrchstr, aircraft['flight']):
+                    result_regex = [True, 0]
 
+        # Is the plane in the point radius for alerting?
+        if result_radius[0] == True and result_altitude[0] == True and result_regex[0] == True:
             # Build a dictionary to add to existing
             tmp2 = {aircraft['hex']: {'distance': result_radius[1], 'altitude': result_altitude[1], 'flight': aircraft['flight']}}
             tmp.update(tmp2)
